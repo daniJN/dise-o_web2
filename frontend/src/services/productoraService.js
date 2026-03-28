67cdc8a1cb7b685dc57e6fdd97ee88a1dbc10352
Daniela <danielaoncedos@gmail.com>
2026-03-27 21:51:22 -0500

    fix: update all services to use env variable

--- frontend/src/services/productoraService.js
+++ frontend/src/services/productoraService.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const API = "http://localhost:4000/api/productoras";
+const URL = `${process.env.REACT_APP_API_URL}/productoras`;
 
 export const getProductoras = () => {
-  return axios.get(API);
+  return axios.get(URL);
 };
 
 export const crearProductora = (data) => {
-  return axios.post(API, data);
+  return axios.post(URL, data);
 };
 
 export const actualizarProductora = (id, data) => {
-  return axios.put(`${API}/${id}`, data);
+  return axios.put(`${URL}/${id}`, data);
 };
 
 export const eliminarProductora = (id) => {
-  return axios.delete(`${API}/${id}`);
+  return axios.delete(`${URL}/${id}`);
 };
--- frontend/src/services/tipoService.js
+++ frontend/src/services/tipoService.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const API = "http://localhost:4000/api/tipos";
+const URL = `${process.env.REACT_APP_API_URL}/tipos`;
 
 export const getTipos = () => {
-  return axios.get(API);
+  return axios.get(URL);
 };
 
 export const crearTipo = (data) => {
-  return axios.post(API, data);
+  return axios.post(URL, data);
 };
 
 export const actualizarTipo = (id, data) => {
-  return axios.put(`${API}/${id}`, data);
+  return axios.put(`${URL}/${id}`, data);
 };
 
 export const eliminarTipo = (id) => {
-  return axios.delete(`${API}/${id}`);
+  return axios.delete(`${URL}/${id}`);
 };
